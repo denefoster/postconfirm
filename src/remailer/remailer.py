b345c664453ce0bb226351f6d216418fcf8c3cdc
dane foster <dene@slush.ca>
2026-03-14 15:09:55 +1300

    .

--- src/remailer/remailer.py
+++ src/remailer/remailer.py
@@ -51,7 +51,7 @@ class Remailer:
                 hostname=self.host,
                 port=self.port,
                 local_hostname=self.helo_host,
-                validate_certs=bool(self.validate_certs),
+                validate_certs=False
             ) as smtp:
                 if self.username:
                     await smtp.starttls()
